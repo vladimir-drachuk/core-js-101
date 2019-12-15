6cee504277a164cc626ad09592a58f7a41b59bcf
vladimir-drachuk <vvdra4@gmail.com>
2019-12-15 20:17:04 +0300

    feat: solute 2 files

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -52,9 +52,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  if ((value1 + value2) / 2 > 1.7976931348623157e+307
-  || (value1 + value2) / 2 < -1.7976931348623157e+307) return Infinity;
-  return (value1 + value2) / 2;
+  return value1 / 2 + value2 / 2;
 }
 
 /**
@@ -112,14 +110,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  const a = Math.sqrt(y1 ** 2 + x1 ** 2);
-  const b = Math.sqrt(y2 ** 2 + x2 ** 2);
-  const c = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
-  const result3 = a ** 2 + b ** 2 - c ** 2;
-  const result2 = -2 * a * b;
-  const result1 = result3 / result2;
-  const result = Math.acos(result1);
-  return result;
+  return Math.acos((Math.imul(x1, x2) + Math.imul(y1, y2) / (Math.hypot(x1, y1)
+  * Math.hypot(x2, y2))));
 }
 
 /**
@@ -134,8 +126,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 
@@ -150,8 +142,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -167,8 +159,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -188,8 +180,8 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return (pow === 0) ? num : Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
@@ -209,8 +201,12 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let result = true;
+  for (let i = 2; i < n; i += 1) {
+    if (!(n % i)) result = false;
+  }
+  return result;
 }
 
 /**
@@ -228,8 +224,13 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  let result = +value;
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(result)) {
+    result = def;
+  }
+  return result;
 }
 
 module.exports = {
